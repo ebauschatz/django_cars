ba11da11c6d59dbd3db2fa17b793679bfba93e55
ebauschatz <eaenglish113@gmail.com>
2022-09-21 16:29:19 -0500

    adding update car functionality

--- cars/views.py
+++ cars/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .models import Car
 from .serializers import CarSerializer
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from cars import serializers
 
@@ -19,12 +20,14 @@ def cars_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def car_detail(request, pk):
-    try:
-        car = Car.objects.get(pk = pk)
+    car = get_object_or_404(Car, pk = pk)
+    if request.method == 'GET':
         serializer = CarSerializer(car)
         return Response(serializer.data)
-    except Car.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    return Response(pk)
+    elif request.method == 'PUT':        
+        serializer = CarSerializer(car, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
