ac5da1c901dc49cbc0dc870d072647e23558cb5f
ebauschatz <eaenglish113@gmail.com>
2022-09-21 16:20:45 -0500

    adding ability to get single car by ID

--- cars/views.py
+++ cars/views.py
@@ -4,6 +4,8 @@ from .models import Car
 from .serializers import CarSerializer
 from rest_framework import status
 
+from cars import serializers
+
 @api_view(['GET', 'POST'])
 def cars_list(request):
     if request.method == 'GET':
@@ -16,3 +18,13 @@ def cars_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def car_detail(request, pk):
+    try:
+        car = Car.objects.get(pk = pk)
+        serializer = CarSerializer(car)
+        return Response(serializer.data)
+    except Car.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    return Response(pk)
