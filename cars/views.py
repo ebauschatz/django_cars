d9727c73671153a698803efae121319aca8130e2
ebauschatz <eaenglish113@gmail.com>
2022-09-21 14:48:52 -0500

    admin site updates

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Car
+from .serializers import CarSerializer
 
 @api_view(['GET'])
 def cars_list(request):
-    return Response('ok')
+    cars = Car.objects.all()
+
+    serializer = CarSerializer(cars, many=True)
+    return Response(serializer.data)
