ecdc81644e78b4e21e4a09486219d263e5d5f550
ebauschatz <eaenglish113@gmail.com>
2022-09-21 11:53:51 -0500

    new app and settings, first model

---
+++ cars/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Car(models.Model):
+    make = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    year = models.IntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
